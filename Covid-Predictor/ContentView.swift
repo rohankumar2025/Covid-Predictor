52afae658b17ee8290505d333fac706852726ba4
Rohan Kumar <rohankumar@ucsb.edu>
2022-07-27 14:16:06 -0700

    Initial Commit

---
+++ Covid-Predictor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-Predictor
+//
+//  Created by Rohan Kumar on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid-Predictor/Covid_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_PredictorApp.swift
+//  Covid-Predictor
+//
+//  Created by Rohan Kumar on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Covid_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
